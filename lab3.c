12e9a57a2d9ff856a3044872a0ae0fdc078b41ae
brandonsladek <brandonsladek@gmail.com>
2015-02-25 02:09:04 -0700

    Final product

--- lab3.c
+++ lab3.c
@@ -1,63 +1,125 @@
-// Imports
+// Import standard I/O library
 #include <stdio.h>
-#include <stdbool.h>
 
 // Function prototypes
 int get_input();
-boolean is_valid(int num);
+int is_valid(int num);
 void print_pattern();
 
 // Main function
-void main() {
+int main() {
 
+	// Simple formatting
+	printf("------------------------------------------------------\n");
+	
 	// Good manners
-	printf("Welcome to the Lab3 program for CSCI112.");
+	printf("\nWelcome to the Lab3 program for CSCI112.\n\n");
 	
-	// Get user input
-	int input = get_input();
+	int cont = 1;
 	
-	printf("You entered %d.", input);
+	while (cont == 1) {
 	
-	// Return without errors
-	return(0);
+		// Get user input
+		int input = get_input();
+	
+		printf("\nYou entered %d.\n\n", input);
+	
+		// See function implementation
+		print_pattern(input);
+		
+		printf("\n------------------------------------------------------\n");
+		
+		int answer;
+		
+		// Query user to loop through process again
+		printf("Would you like to build a diamond with another number?\n");
+		printf("Enter 1 for yes, 0 for no.\n\n");
+		
+		// Read in user input
+		scanf("%d", &cont);
+	
+	} // End while loop
+	
+	// Good manners
+	printf("\nGoodbye.\n\n");
 }
 
 // Read in user input, odd integer 1-9 inclusive
 int get_input() {
+
 	int input = 0;
 	
-	while (is_valid(input) == 0) {
-	
+	// Repeatedly query user until input is valid
+	do {
 		printf("Enter an odd number 1-9 inclusive >>> ");
         
         // Save user input to input variable
 		scanf("%d", &input);
-	}
+		
+		// Input must be an odd integer between 1 and 9 inclusive
+		if (input < 1) {
+			printf("\nYou have entered a number less than 1. Please try again.\n\n");
+		} else if (input > 9) {
+			printf("\nYou have entered a number greater than 9. Please try again.\n\n");
+		} else if (input % 2 == 0) {
+			printf("\nYou have entered an even number. Please try again.\n\n");
+		}
+		
+	// Check validity of number
+	} while (is_valid(input) == 0);
+	
+	// Return valid input
 	return input;
 }
 
 // Return 1 if number is valid, 0 otherwise
 int is_valid(int num) {	
 
+	// Number must be an odd integer between 1 and 9 inclusive
 	if (num >= 1 && num <= 9) {
 		if (num % 2 != 0) {
-		printf("%d is valid.", num);
 		return 1;
 	}
-	
-  } else if (num < 1) {
-  		printf("You have entered a number less than one. Please try again.");
-  		return 0;
-  } else if (num > 9) {
-  		printf("You have entered a number greater than nine. Please try again.");
-  		return 0;
-  } else if (num % 2 = 0) {
-  		printf("You have entered an even number. Please try again.");
-  		return 0;
+  } 
+  return 0;
   } // End of is_valid function
   
-  // Return false by default
-  return 0;
-}
+void print_pattern(int num) {
 
-void print_pattern() {}
+	// Print the top half of the diamond
+	
+	int i;
+	
+	for (i = 1; i <= num; i += 2) {
+		printf("%*s", num - i, "");
+		
+		int j = 1;
+		
+		// While loop to print numbers
+		while (j <= i) {
+			printf("%d ", j);
+			j++;
+		}
+		printf("%*s\n", num - i, "");
+	} // End for loop
+	
+	// Print the bottom half of the diamond
+	
+	int z;
+	
+	for (z = num-2; z >= 1; z -= 2) {
+		printf("%*s", num - z, "");
+		
+		int k = 1;
+		
+		// While loop to print numbers 
+		while (k <= z) {
+			printf("%d ", k);
+			k++;
+		}
+		printf("%*s\n", num - z, "");
+	} // End for loop
+	
+} // End print_pattern function
+
+// End of file
